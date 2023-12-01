852692a92087317a58f19b17dbd483036f8624df
Mark Manders <mmjmanders@gmail.com>
2023-12-01 19:32:07 +0100

    Fixed packaging

--- prepare-release.js
+++
@@ -1,7 +0,0 @@
-import { copyFile } from 'node:fs/promises';
-
-(async () => {
-  await copyFile('package.json', 'dist/package.json');
-  await copyFile('README.md', 'dist/README.md');
-  await copyFile('LICENSE.md', 'dist/LICENSE.md');
-})();
